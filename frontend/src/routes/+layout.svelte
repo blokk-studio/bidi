0aaec767b6e0ef7643614a43bd124e6865439e73
aptt <git@aptt.ch>
2024-12-16 16:29:43 +0100

    feat(frontend): 🎸 add root layout & move dashboard link to nav in header

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { children } = $props()
+</script>
+
+<header>
+	<nav>
+		<a data-sveltekit-preload-data="tap" href="/dashboard">dashboard</a>
+	</nav>
+</header>
+
+{@render children()}
