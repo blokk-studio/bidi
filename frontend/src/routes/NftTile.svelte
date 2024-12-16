0bb42f841554ffe305f915674a704641b54518e9
aptt <git@aptt.ch>
2024-12-16 16:49:15 +0100

    feat(frontend): 🎸 display nfts on the landing page

---
+++ frontend/src/routes/NftTile.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { Nft } from '$lib/nft'
+	let { serialNumber, name, imageUrl }: Pick<Nft, 'imageUrl' | 'name' | 'serialNumber'> = $props()
+</script>
+
+<div class="nftTile">
+	<span class="serialNumber">#{serialNumber}</span>
+	<span class="name">{name}</span>
+	<img src={imageUrl} alt="NFT image of {name}" class="image" />
+</div>
+
+<style>
+	.nftTile {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		grid-template-areas:
+			'image image'
+			'serial name';
+		column-gap: 1rem;
+		row-gap: 0.75rem;
+	}
+
+	.serialNumber {
+		grid-area: serial;
+	}
+
+	.name {
+		grid-area: name;
+	}
+
+	.image {
+		grid-area: image;
+		width: 100%;
+	}
+</style>
