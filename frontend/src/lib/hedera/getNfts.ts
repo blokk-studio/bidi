2497c26b2581ca41b600de39acab4eeb2c1593de
aptt <git@aptt.ch>
2024-12-18 11:32:35 +0100

    feat(frontend): 🎸 add optional account id to nft getter

--- frontend/src/lib/hedera/getNfts.ts
+++ frontend/src/lib/hedera/getNfts.ts
@@ -1,12 +1,13 @@
 import { PUBLIC_IPFS_GATEWAY_BASE_URL } from '$env/static/public'
 import type { IpfsUri, Nft } from '$lib/nft'
-import { LedgerId, TokenId } from '@hashgraph/sdk'
+import { AccountId, LedgerId, TokenId } from '@hashgraph/sdk'
 import { nftUtils as getNftUtils } from '@tikz/hedera-mirror-node-ts'
 import { MirrorNodeClient } from './MirrorNodeClient'
 
 export type GetNfts = (options: {
 	ledgerId: LedgerId
 	tokenId: TokenId
+	accountId?: AccountId
 	fetch?: typeof globalThis.fetch
 }) => Promise<Nft[]>
 
@@ -25,7 +26,11 @@ export const getNfts: GetNfts = async (options): Promise<Nft[]> => {
 	try {
 		const client = MirrorNodeClient.newFromLedgerId(options.ledgerId, { fetch: fetch_ })
 		const nftUtils = getNftUtils(client)
-		const nftsResponse = await nftUtils.NFTs.setTokenId(options.tokenId.toString()).get()
+		const nftsRequest = nftUtils.NFTs.setTokenId(options.tokenId.toString())
+		if (options.accountId) {
+			nftsRequest.setAccountId(options.accountId.toString())
+		}
+		const nftsResponse = await nftsRequest.get()
 		const nftPromises = nftsResponse.nfts.map(async (nft): Promise<Nft> => {
 			const serialNumber = nft.serial_number
 			// attempt to decode metadata
