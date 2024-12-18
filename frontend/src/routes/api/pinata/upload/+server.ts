271a507ab240cf5ef2f9dc0ea009ac25830a59c4
Marko Gracin <marko@appswithlove.com>
2024-12-18 12:36:20 +0100

    fix(frontend): 🐛 work in progress, fixing ipfs metadata

--- frontend/src/routes/api/pinata/upload/+server.ts
+++ frontend/src/routes/api/pinata/upload/+server.ts
@@ -4,41 +4,46 @@ import { pinata } from '$lib/pinata/pinata.server'
 import { json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 
-import { PINATA_GROUP_ID } from '$env/static/private'
-
 export const POST: RequestHandler = async ({ request }) => {
+	if (typeof window !== 'undefined') {
+		throw new Error('This API route should only run on the server.')
+	}
+
 	try {
 		const formData = await request.formData()
-
-		// todo we wont need file uplaod in the future, as the image will be generated randomly!
-		const uploadedFile = formData.get('file') as File
 		const metadata = JSON.parse(formData.get('metadata') as string)
 
-		if (!uploadedFile?.name || uploadedFile.size === 0) {
-			return json({ error: 'You must provide a file to upload' }, { status: 400 })
-		}
+		// Upload static image todo generate image later on, adding this pre-uploaded url for now
+		// const imageUpload = await pinata.upload.file(staticFile).group(PINATA_GROUP_ID)
+		const imageIpfsUrl = `ipfs://bafkreigwfw2aj7l2nrtyo7a3ias4ui2zdyfdj3zkhetqmioxmhk3yeyxem`
 
-		// Upload image todo for some reason the group id gets ignored?
-		const imageUpload = await pinata.upload.file(uploadedFile).group(PINATA_GROUP_ID)
-
-		const imageIpfsUrl = `ipfs://${imageUpload.IpfsHash}`
-
-		// Create and upload metadata (containing IPFS image url)
 		const fullMetadata = {
-			...metadata,
+			name: 'demo.jpg',
+			creator: 'BiDi Company',
+			description: 'Biodiversity Certificate Metadata',
 			type: 'image/jpg',
 			format: 'none',
+			properties: {
+				latitude: metadata.latitude,
+				longitude: metadata.longitude,
+				typeOfNaturalObject: metadata.typeOfNaturalObject,
+				locationOwner: metadata.locationOwner,
+				operationsManager: metadata.operationsManager,
+				dateOfWork: metadata.dateOfWork,
+				typeOfWork: metadata.typeOfWork,
+				effectOnBiodiversity: metadata.effectOnBiodiversity,
+			},
 			image: imageIpfsUrl,
 		}
 
-		// Upload metadata to IPFS
 		const metadataUpload = await pinata.upload.json(fullMetadata)
+
 		const metadataUrl = `ipfs://${metadataUpload.IpfsHash}`
 
 		return json({
-			imageUrl: await pinata.gateways.convert(imageUpload.IpfsHash),
-			metadataUrl, // this is the only thing that matters id say, and we will pass this as metadata on mint process
-			imageIpfsHash: imageUpload.IpfsHash,
+			// imageUrl: await pinata.gateways.convert(imageUpload.IpfsHash),
+			metadataUrl,
+			// imageIpfsHash: imageUpload.IpfsHash,
 			metadataIpfsHash: metadataUpload.IpfsHash,
 		})
 	} catch (throwable) {
