dedfa2e95c74a8dd1ea4ee3f3d59b20f6051bb95
aptt <git@aptt.ch>
2025-01-06 14:31:01 +0100

    feat(frontend): 🎸 extract navigation link style & apply it on dashboard

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import '../app.css'
 	import '$lib/css/colors.css'
 	import containerStyles from '$lib/css/container.module.css'
+	import navigationLinkStyles from '$lib/css/navigationLink.module.css'
 
 	let { children } = $props()
 </script>
@@ -13,8 +14,8 @@
 
 <header>
 	<nav class={containerStyles.container}>
-		<a href="/" class="navigationLink">BIDI</a>
-		<a href="/dashboard" class="navigationLink">Dashboard</a>
+		<a href="/" class={navigationLinkStyles.navigationLink}>BIDI</a>
+		<a href="/dashboard" class={navigationLinkStyles.navigationLink}>Dashboard</a>
 	</nav>
 </header>
 
@@ -42,30 +43,5 @@
 		position: relative;
 		background: linear-gradient(to bottom right, #f6d8d5, transparent, #f5f5f5);
 		min-height: 100vh;
-	}
-
-	.navigationLink {
-		transition-duration: 100ms;
-		transition-property: text-decoration-color;
-		transition-timing-function: ease-out;
-		color: var(--colorOrange0);
-		font-weight: 600;
-		text-decoration-line: underline;
-		text-decoration-color: transparent;
-		text-decoration-thickness: 0.15em;
-		text-underline-offset: 0.25em;
-
-		@media (hover: hover) {
-			&:hover {
-				text-decoration-color: var(--colorOrange0);
-			}
-		}
-
-		@media (hover: none) {
-			&:active {
-				transition-duration: 20ms;
-				text-decoration-color: var(--colorOrange0);
-			}
-		}
 	}
 </style>
