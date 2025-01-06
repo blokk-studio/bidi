abdcf4d77032c1704310092be08ec71afa1b7213
aptt <git@aptt.ch>
2025-01-06 14:15:56 +0100

    feat(frontend): 🎸 add nft colors as css variables & style header links

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/state'
 	import '../app.css'
+	import '$lib/css/colors.css'
 
 	let { children } = $props()
 </script>
@@ -11,8 +12,8 @@
 
 <header>
 	<nav>
-		<a href="/">Landing Page</a>
-		<a href="/dashboard">Dashboard</a>
+		<a href="/" class="navigationLink">BIDI</a>
+		<a href="/dashboard" class="navigationLink">Dashboard</a>
 	</nav>
 </header>
 
@@ -39,5 +40,30 @@
 		position: relative;
 		background: linear-gradient(to bottom right, #f6d8d5, transparent, #f5f5f5);
 		min-height: 100vh;
+	}
+
+	.navigationLink {
+		transition-duration: 100ms;
+		transition-property: text-decoration-color;
+		transition-timing-function: ease-out;
+		color: var(--colorOrange0);
+		font-weight: 600;
+		text-decoration-line: underline;
+		text-decoration-color: transparent;
+		text-decoration-thickness: 0.15em;
+		text-underline-offset: 0.25em;
+
+		@media (hover: hover) {
+			&:hover {
+				text-decoration-color: var(--colorOrange0);
+			}
+		}
+
+		@media (hover: none) {
+			&:active {
+				transition-duration: 20ms;
+				text-decoration-color: var(--colorOrange0);
+			}
+		}
 	}
 </style>
