f0b2dac748969020b56367cf7688a425b2288deb
aptt <git@aptt.ch>
2025-01-15 15:16:38 +0100

    feat(frontend): 🎸 add component to render multiline strings in html

---
+++ frontend/src/lib/components/MultilineText.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { text }: { text: string } = $props()
+
+	const lines = $derived(text.split(/\n/g))
+</script>
+
+{#each lines as line}
+	<span class="line">{line}</span>
+{/each}
+
+<style>
+	.line {
+		display: block;
+	}
+</style>
