9c78bb84b955fb1ba089047a7779a4d88cf21190
aptt <git@aptt.ch>
2025-01-15 15:17:02 +0100

    feat(frontend): 🎸 properly display multiline text in mission teaser

--- frontend/src/routes/MissionTeaser.svelte
+++ frontend/src/routes/MissionTeaser.svelte
@@ -3,6 +3,7 @@
 	import type { BidiCertificate } from '$lib/certificate'
 	import Details from 'lucide-svelte/icons/arrow-right'
 	import type { Nft } from '$lib/nft'
+	import MultilineText from '$lib/components/MultilineText.svelte'
 
 	let {
 		name,
@@ -18,7 +19,11 @@
 
 	<time datetime={dateOfWork}>{new Date(dateOfWork).toLocaleDateString()}</time>
 
-	<p>{typeOfWork}</p>
+	{#if typeOfWork}
+		<p>
+			<MultilineText text={typeOfWork} />
+		</p>
+	{/if}
 
 	<a
 		href="/certificates/{serialNumber}"
