88c5766183c35014b4560f562e06e2606a77480d
aptt <git@aptt.ch>
2025-01-20 17:34:11 +0100

    feat(frontend): 🎸 use type returned by mission getter on home page

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,10 +1,10 @@
 import { nftTokenId } from '$lib/deployment'
-import { getMissions, type Mission } from '$lib/directus'
+import { getMissions } from '$lib/directus'
 import { getNfts } from '$lib/hedera/getNfts'
 import { LedgerId } from '@hashgraph/sdk'
 
 export const load = async ({ fetch }) => {
-	let missions: Pick<Mission, 'title' | 'date' | 'type_of_work'>[] = []
+	let missions: Awaited<ReturnType<typeof getMissions>> = []
 	try {
 		missions = await getMissions({ fetch })
 	} catch {
