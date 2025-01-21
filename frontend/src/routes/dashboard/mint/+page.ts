114bc355a925b6c8d610ca5425c65a206534042f
aptt <git@aptt.ch>
2025-01-21 16:26:27 +0100

    feat(frontend): 🎸 load missions on mint page & pass to cert creation

--- frontend/src/routes/dashboard/mint/+page.ts
+++ frontend/src/routes/dashboard/mint/+page.ts
@@ -1,5 +1,15 @@
-export const load = () => {
+import { getMissions } from '$lib/directus'
+
+export const load = async ({ fetch }) => {
+	let missions: Awaited<ReturnType<typeof getMissions>> | undefined = undefined
+	try {
+		missions = await getMissions({ fetch })
+	} catch {
+		// ignore errors
+	}
+
 	return {
+		missions,
 		metadataTitle: 'Create a certificate - BIDI',
 	}
 }
