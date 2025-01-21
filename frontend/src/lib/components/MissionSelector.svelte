cecfe2fbc89b36a50401cb49e4528e11ff55f649
aptt <git@aptt.ch>
2025-01-21 16:23:50 +0100

    feat(frontend): 🎸 add basic mission selector component

---
+++ frontend/src/lib/components/MissionSelector.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Mission } from '$lib/directus'
+
+	let {
+		selectedMission = $bindable(),
+		missions,
+		required,
+	}: {
+		selectedMission?: Mission
+		missions: Mission[]
+		required?: boolean
+	} = $props()
+</script>
+
+<fieldset class="missionGrid">
+	<legend>Select a Mission</legend>
+
+	{#each missions as mission}
+		<label>
+			<input type="radio" name="mission" bind:group={selectedMission} value={mission} {required} />
+
+			{mission.title}
+		</label>
+	{/each}
+</fieldset>
+
+<style>
+	.missionGrid {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		column-gap: 1rem;
+		row-gap: 0.5rem;
+	}
+</style>
