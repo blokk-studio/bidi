ca55d6525cf9eac6836931e552eb1922336f6287
aptt <git@aptt.ch>
2025-01-22 14:10:55 +0100

    feat(frontend): 🎸 display claimed nfts in compact list

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import NftTile from '$lib/components/NftTile.svelte'
 	import { nftTokenId } from '$lib/deployment'
 	import HashConnectLoader from '$lib/hashconnect/HashConnectLoader.svelte'
 	import { getNfts } from '$lib/hedera/getNfts'
 	import containerStyles from '$lib/css/container.module.css'
+	import navigationLinkStyles from '$lib/css/navigationLink.module.css'
 </script>
 
 <main class={containerStyles.container}>
@@ -19,15 +19,63 @@
 				{#if !nfts.length}
 					<p>You don't have any certificates.</p>
 				{:else}
-					<ul>
-						{#each nfts as nft}
-							<li>
-								<NftTile {...nft} />
-							</li>
-						{/each}
-					</ul>
+					<div role="presentation" class="nftListContainer">
+						<ul class="nftList">
+							{#each nfts as nft}
+								<li>
+									<a
+										href="/certificates/{nft.serialNumber}"
+										class={navigationLinkStyles.navigationLink}
+									>
+										<figure class="nftListItem">
+											<img
+												src={nft.imageUrl}
+												alt="Image of certificate {nft.name}"
+												class="nftTile"
+											/>
+
+											<div role="presentation">
+												<span class="nftSerialNumber">#{nft.serialNumber}</span>
+
+												{nft.name}
+											</div>
+										</figure>
+									</a>
+								</li>
+							{/each}
+						</ul>
+					</div>
 				{/if}
 			{/await}
 		{/snippet}
 	</HashConnectLoader>
 </main>
+
+<style>
+	.nftListContainer {
+		container-name: nfts;
+		container-type: inline-size;
+	}
+
+	.nftList {
+		display: grid;
+		grid-template-columns: 1fr;
+		gap: 1rem;
+		@container nfts (min-width: 20rem) {
+			grid-template-columns: repeat(2, 1fr);
+		}
+		@container nfts (min-width: 40rem) {
+			grid-template-columns: repeat(3, 1fr);
+		}
+	}
+
+	.nftListItem {
+		display: grid;
+		gap: 0.5rem;
+	}
+
+	.nftSerialNumber {
+		margin-right: 0.75rem;
+		font-family: 'Andale Mono', monospace;
+	}
+</style>
