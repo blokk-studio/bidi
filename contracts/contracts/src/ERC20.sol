cbde11f0a08ab0b24edb609342d6f4ae3b9445fa
Marko Gracin <marko@appswithlove.com>
2025-01-28 14:35:22 +0100

    feat(contracts): 🎸 set starting point for ERC20 contract

--- contracts/contracts/src/ERC20.sol
+++ contracts/contracts/src/ERC20.sol
@@ -3,7 +3,17 @@
 pragma solidity ^0.8.22;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+contract Bidi is ERC20, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+    ERC20("Bidi", "BIDI")
+    Ownable(initialOwner)
+    ERC20Permit("Bidi")
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
