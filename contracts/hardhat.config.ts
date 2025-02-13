54b737c1bf363ebd69f7313a5a37e167507c461e
Marko Gracin <marko@appswithlove.com>
2025-02-13 12:12:14 +0100

    feat(contracts): 🎸 bump solidity version

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,13 +3,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.18",
-    paths: {
-        sources: "./contracts",
-        tests: "./test",
-        cache: "./cache",
-        artifacts: "./artifacts"
-    }
+  solidity: "0.8.28",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
 
 export default config;
